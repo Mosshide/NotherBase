d61941e3055d08b87afeed0d83ffa45645fecdda
Wyatt Sushinsky <wyattsushi@gmail.com>
2024-03-02 00:52:53 -0800

    global crud

--- world/pages/portfolio/save-example.js
+++ world/pages/portfolio/save-example.js
@@ -31,26 +31,23 @@ export default async (req, user) => {
     }
     migrate("portfolio-examples", "title", 7);
 
-    let basicCrud = async function (service, individual) {
-        if (!user.loggedIn()) {
-            return `You must be logged in to save a(n) ${individual}.`;
-        }
-        else if (!req.body.item?.id) {
-            let spirit = await req.db.Spirit.create(service, {}, user.id);
+    let basicGlobalCrud = async function (service, individual) {
+        if (!req.body.item?.id) {
+            let spirit = await req.db.Spirit.create(service, {}, null);
             spirit.addBackup(req.body.item.data);
             await spirit.commit();
             return `${individual} created.`;
         }
         else if (req.body.deleting) {
-            let del = await req.db.Spirit.delete(service, {}, user.id, req.body.item.id);
+            let del = await req.db.Spirit.delete(service, {}, null, req.body.item.id);
             return `${del} deleted.`;
         }
         else {
-            let spirit = await req.db.Spirit.recallOne(service, user.id, {}, req.body.item.id);
+            let spirit = await req.db.Spirit.recallOne(service, null, {}, req.body.item.id);
             spirit.addBackup(req.body.item.data);
             await spirit.commit();
             return `${individual} saved.`;
         }
     }
-    return await basicCrud("portfolio-examples", "Example");
+    return await basicGlobalCrud("portfolio-examples", "Example");
 }
