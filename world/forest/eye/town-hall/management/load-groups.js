9a89d80e26dcbf77fc67c27370db216c7fb20c2c
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2024-05-10 14:07:07 -0700

    groups usernames fix

--- world/forest/eye/town-hall/management/load-groups.js
+++ world/forest/eye/town-hall/management/load-groups.js
@@ -8,48 +8,32 @@ export default async function (req, user) {
             ...settings
         }
 
-        let groups = await req.db.Spirit.recallAll("group");
-        let inGroups = [];
-        
-        for (let i = 0; i < groups.memory.length; i++) {
-            if (!groups.memory[i].data) groups.memory.splice(i, 1);
-            else {
-                if (Array.isArray(groups.memory[i].data.members)) {
-                    let userFound = false;
-    
-                    if (settings.getMembers) {
-                        for (let j = 0; j < groups.memory[i].data.members.length; j++) {
-                            let findUser = null;
-                            if (groups.memory[i].data.members[j].id == `${user.id}`) {
-                                userFound = true;
-                                groups.memory[i].data.members[j].name = user.memory.data.username;
-                            }
-                            else {
-                                findUser = await req.db.User.recallOne(null, null, groups.memory[i].data.members[j].id);
-                                if (findUser) groups.memory[i].data.members[j].name = findUser.memory.data.username;
-                            }
-                        }
-                    }
-                    
-                    if (userFound) {
-                        if (settings.getJoinRequests) {
-                            if (!Array.isArray(groups.memory[i].data.joinRequests)) groups.memory[i].data.joinRequests = [];
-        
-                            for (let j = 0; j < groups.memory[i].data.joinRequests.length; j++) {
-                                let findUser = await req.db.User.recallOne(null, null, groups.memory[i].data.joinRequests[j].id);
-                                
-                                groups.memory[i].data.joinRequests[j].name = findUser.memory.data.username;
-                            }
-                        }
-
-                        inGroups.push(groups.memory[i]);
-                    }
+        let groups = await req.db.Spirit.recallAll("group", null, {
+            members: {
+                $elemMatch: {
+                    id: `${user.id}`
                 }
-                else groups.memory[i].data.members = [];
             }
-        }
+        });
 
-        await groups.commit();
+        let foundUsers = {};
+        let inGroups = [];
+
+        for (let i = 0; i < groups.memory.length; i++) {
+            for (let j = 0; j < groups.memory[i].data.members.length; j++) {
+                if (groups.memory[i].data.members[j].id == `${user.id}`) {
+                    groups.memory[i].data.members[j].name = user.username;
+                }
+                else {
+                    if (!foundUsers[groups.memory[i].data.members[j].id]) {
+                        foundUsers[groups.memory[i].data.members[j].id] = await req.db.User.recallOne(null, null, groups.memory[i].data.members[j].id);
+                    }
+                    if (foundUsers[groups.memory[i].data.members[j].id]) groups.memory[i].data.members[j].name = foundUsers[groups.memory[i].data.members[j].id].username;
+                }
+            }
+
+            inGroups.push(groups.memory[i]);
+        }
 
         return inGroups;
     }
