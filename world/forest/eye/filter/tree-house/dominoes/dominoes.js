871cec3d02d8248e15daa14b0ebdd455aaa454a8
Wyatt Sushinsky <wyattsushi@gmail.com>
2024-08-09 09:43:02 -0700

    dominoes restart

--- world/forest/eye/filter/tree-house/dominoes/dominoes.js
+++ world/forest/eye/filter/tree-house/dominoes/dominoes.js
@@ -220,100 +220,6 @@ class Board extends Element {
             this.updateOptions(bone);
         }
     }
-
-    takeBackPlacement() {
-        this.updateOptions(null);
-    }
-
-    finishPlacement(bone, targetBone) {
-        this.bonesOnBoard.push(bone);
-        this.$board.append(bone.$div);
-        
-        if (targetBone) {
-            targetBone.attach(bone);
-            for (let i = 0; i < this.ends.length; i++) {
-                if (targetBone == this.ends[i]) this.ends.splice(i, 1, bone);
-            }
-        }
-        else {
-            bone.moveTo([0, 0]);
-            this.ends.push(bone);
-        }
-
-        this.$board.removeClass("big-bone-time");
-
-        return 
-    }
-
-    grabPoints() {
-        let points = 0;
-
-        for (let i = 0; i < this.ends.length; i++) {
-            points += this.ends[i].values[0];
-        }
-
-        if (points % 5 === 0) return points;
-        else return 0;
-    }
-
-    updateOptions(bone) {
-        for (let i = 0; i < this.bonesOnBoard.length; i++) {
-            this.bonesOnBoard[i].$div.removeClass("highlighted");
-        }
-
-        if (bone !== null) {
-            if (this.bonesOnBoard.length > 0) {
-                for (let i = 0; i < this.ends.length; i++) {
-                    if (bone.fits(this.ends[i])) this.ends[i].$div.addClass("highlighted");
-                }
-            }
-            else this.$board.addClass("big-bone-time");
-        }
-    }
-
-    domino() {
-        let total = 0;
-
-        for (let i = 0; i < this.boneyard.length; i++) {
-            total += this.boneyard[i].values[0] + this.boneyard[i].values[1];
-        }
-
-        this.points = total - (total % 5);
-
-        return this.grabPoints();
-    }
-
-    click = (e) => {
-        console.log("clicked board");
-        if (this.waiting && this.turn === 1) {
-            if (this.players[1].heldBone !== null) {
-                this.board.finishPlacement(this.players[1].playBone());
-                this.players[1].addPoints(this.board.grabPoints());
-                this.turn = 0;
-                this.startTurn();
-            }
-        }
-    }
-}
-
-// a score class
-class Score extends Text {
-    constructor() {
-        super("div", {
-            defaultClasses: "score"
-        });
-        this.points = 0;
-    }
-
-    addPoints(points) {
-        this.points += points;
-        this.setValue(this.points);
-    }
-
-    clear() {
-        this.value = 0;
-        this.setValue(this.points);
-    }
 }
 
 // a hand class
@@ -326,9 +232,6 @@ class Hand extends Element {
 
         this.game = game;
         this.heldBone = null;
-        this.score = this.addChild(new Score());
-        this.score.clear();
-        this.player = new Bot(this.game, "Bot");
         this.state = "out-of-game";
     }
 
@@ -336,64 +239,6 @@ class Hand extends Element {
         bone.location = "hand";
         this.addChild(bone);
         return bone.values;
-    }
-
-    selectBone(bone) {
-        for (let i = 0; i < this.hand.length; i++) {
-            if (this.hand[i] !== bone) this.hand[i].unselect();
-        }
-
-        this.board.updateOptions(bone);
-    }
-
-    checkForDraw(board, bigBone = false) {
-        this.options = [];
-
-        if (!bigBone) {
-            for (let i = 0; i < this.player.board.ends.length; i++) {
-                for (let j = 0; j < this.hand.length; j++) {
-                    if (this.hand[j].fits(this.player.board.ends[i])) this.options.push(j);
-                }
-            }
-
-            if (this.options.length <= 0) {
-                let bone = this.player.board.yard.getBone();
-                if (bone !== null) {
-                    this.draw(bone);
-                    this.checkForDraw();
-                }
-            }
-        }
-        else {
-            let max = -1;
-            let chosen = -1;
-
-            for (let j = 0; j < this.hand.length; j++) {
-                if (this.hand[j].values[0] === this.hand[j].values[1]) {
-                    if (this.hand[j].values[0] > max) {
-                        max = this.hand[j].values[0];
-                        chosen = j;
-                    }
-                }
-            }
-
-            this.options = [chosen];
-        }
-
-        this.updateOptionsVisuals();
-    }
-
-    updateOptionsVisuals(only = null) {
-        for (let i = 0; i < this.hand.length; i++) {
-            this.hand[i].unHighlight();
-
-            if (!only) {
-                for (let j = 0; j < this.options.length; j++) {
-                    if (this.options[j] === i) this.hand[i].highlight();
-                }
-            }
-            else this.hand[only].highlight();
-        }
     }
 
     chooseBone(bone) {
@@ -410,94 +255,11 @@ class Hand extends Element {
 
         return null;
     }
-
-    playBone() {
-        for (let i = 0; i < this.options.length; i++) {
-            if (this.hand[this.options[i]] == this.heldBone) {
-                this.heldBone.unHold();
-                this.heldBone.unHighlight();
-                this.heldBone = null;
-                return this.hand.splice(this.options[i], 1)[0];
-            }
-        }
-
-        return null;
-    }
     
     unChooseBone(bone) {
         bone.unHold();
         this.heldBone = null;
         this.updateOptionsVisuals();
-    }
-
-    checkForDomino() {
-        if (this.hand.length <= 0) return true;
-        else return false;
-    }
-}
-
-class Player extends Element {
-    constructor(dominoGame, name) {
-        super("div", {
-            defaultClasses: "player"
-        });
-        this.name = name;
-        this.dominoGame = dominoGame;
-        this.hand = null;
-        this.choice = {
-            bone: null,
-            target: null,
-            pole: -1
-        };
-        this.options = [];
-        this.waiting = false;
-    }
-
-    startTurn(bigBone = false) {
-        this.$div.addClass("turn-to-play");
-        this.hand.checkForDraw(this.board, bigBone);
-        this.waiting = true;
-    }
-
-    endTurn() {
-        this.$div.removeClass("turn-to-play");
-        this.score.addPoints(this.board.grabPoints());
-    }
-
-    displayWinner(player) {
-        $(".opponent-hand").text(`${player} won!`);
-    }
-
-    displayLoser(player) {
-        $(".player-hand").text(`${player} lost!`);
-    }
-}
-
-class Bot extends Player {
-    constructor(game, name) {
-        super(game, name);
-    }
-
-    autoTurn(bigbone = false) {
-        this.$handDiv.addClass("turn-to-play");
-        this.checkForDraw(bigBone);
-        this.updateOptionsVisuals();
-
-        //
-        let chosen = -1;
-        if (bigbone) chosen = this.options[0];
-        else chosen = this.options[Math.floor(Math.random() * this.options.length)];
-
-        this.choice = {
-            bone: this.hand[chosen],
-            target: "auto",
-            pole: -1
-        };
-
-        //
-        this.$handDiv.removeClass("turn-to-play");
-        this.board.place(this.choice);
-        this.addPoints(this.board.grabPoints());
     }
 }
 
@@ -506,22 +268,10 @@ class DominoGame extends Container {
         super({
             defaultClasses: "table"
         });
-        this.turn = -1;
         this.players = null;
-        this.scoreToWin = 100;
-
-        this.buttons = this.addChild(new Buttons({ id: "dominoes-ui" }));
-        this.buttons.addButton(new Button("play", (e, self) => { this.startGame(); }, { placeholder: "Play" }));
 
         this.hands = [this.addChild(new Hand(this, "player-1")), this.addChild(new Hand(this, "player-2"))];
         this.board = this.addChild(new Board(this));
-    }
-
-    startGame() {
-        console.log("Starting Game");
-        this.buttons.hideButton();
-
-        this.startRound();
     }
 
     startRound() {
@@ -555,52 +305,6 @@ class DominoGame extends Container {
         }
 
         this.startTurn(true);
-    }
-
-    startTurn(bigBone = false) {
-        console.log("Starting Turn");
-        
-        if (this.turn === 0) {
-            this.players[0].autoTurn(bigBone);
-            // this.players[0].checkForDraw(bigBone);
-            // this.board.place(this.players[0].autoTurn(), bigBone);
-            // this.players[0].addPoints(this.board.grabPoints());
-            // if (this.players[0].checkForDomino()) this.resolveDomino(0);
-            // this.players[0].endTurn();
-
-            this.turn = 1;
-            this.startTurn();
-        }
-        else if (this.turn === 1) {
-            this.players[1].startTurn(bigBone);
-        }
-    }
-
-    endTurn() {
-        console.log("Ending Turn");
-        if (this.players[this.turn].checkForDomino()) this.resolveDomino();
-        this.turn = Math.abs(this.turn - 1);
-        startTurn();
-    }
-
-    resolveDomino() {
-        console.log("Resolving Domino");
-        this.players[this.turn].addPoints(this.board.domino(), this.players[Math.abs(this.turn - 1)].domino());
-        for (let i = 0; i < this.players.length; i++) {
-            this.players[i].clear();
-        }
-        this.checkForWin();
-        this.startRound();
-    }
-
-    checkForWin() {
-        console.log("Checking for Win");
-        if (this.players[this.turn].score >= this.scoreToWin) {
-            this.players[this.turn].displayWinner();
-            this.players[Math.abs(this.turn - 1)].displayLoser();
-
-            $("#play").removeClass("invisible");
-        }
     }
 }
 
