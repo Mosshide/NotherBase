77db0eff8e6bdb3221051a39fa6c0aea0ec0544a
Wyatt Sushinsky <wyattsushi@gmail.com>
2024-08-18 14:35:07 -0700

    many bases update

--- notherbase.js
+++ notherbase.js
@@ -1,10 +1,21 @@
 import NotherBaseFS from "notherbase-fs";
-import { fileURLToPath } from 'node:url';
-const __dirname = fileURLToPath(new URL('./world', import.meta.url));
-
 import nkjvBible from "./globals/NKJV.js";
+import { fileURLToPath } from "url";
 
-const notherBaseFS = new NotherBaseFS(__dirname, { nkjvBible }, { 
-    siteTitle: "NotherBase",
-    favicon: fileURLToPath(new URL("./public/img/the-front/logo.png", import.meta.url))
+const notherBaseFS = new NotherBaseFS({ nkjvBible }, {
+    notherbase: {
+        title: 'NotherBase',
+        directory: fileURLToPath(new URL('./world', import.meta.url)),
+        icon: '/public/img/the-front/logo.png'
+    },
+    pebblewireless: {
+        title: 'Pebble Wireless',
+        directory: fileURLToPath(new URL('./pebblebase', import.meta.url)),
+        icon: '/public/img/drum.png'
+    },
+    loveincoflewiscounty: {
+        title: 'Love INC of Lewis County',
+        directory: fileURLToPath(new URL('./loveinclcbase', import.meta.url)),
+        icon: '/public/img/favicon.ico'
+    }
 });
