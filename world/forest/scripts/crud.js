b6beb5bdb0c6dd6b429239192c5e25c807aadf70
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2026-01-16 00:52:44 -0800

    it admin and bible fix

--- world/forest/scripts/crud.js
+++ world/forest/scripts/crud.js
@@ -2,7 +2,7 @@ export default async function (req, user, service, individual) {
     if (!req.session.currentUser) {
         return `You must be logged in to save a(n) ${individual}.`;
     }
-    else if (!req.body.item?.id) {
+    else if (!req.body.item?.id || req.body.item.id === "new") {
         let spirit = await req.db.Spirit.create(service, {}, user.memory._id);
         spirit.addBackup(req.body.item.data);
         await spirit.commit();
@@ -12,7 +12,7 @@ export default async function (req, user, service, individual) {
         let del = await req.db.Spirit.delete(service, user.memory._id, {}, req.body.item.id);
         return `${del} deleted.`;
     }
-    else {
+    else {       
         let spirit = await req.db.Spirit.recallOne(service, user.memory._id, {}, req.body.item.id);
         spirit.addBackup(req.body.item.data);
         await spirit.commit();
--- world/the-front/it-admin/save-it.js
+++ world/the-front/it-admin/save-it.js
@@ -3,8 +3,6 @@ import basicCrud from "../../forest/scripts/crud.js";
 export default async (req, user) => {
     if (user.memory.data.authLevels.includes("ITAD")) {
         let userToFind = await req.db.Spirit.recallOne("user", null, { username: req.body.which });
-        console.log(req);
-        
         if (userToFind) return await basicCrud(req, userToFind, "it", "Entry");
         else return "not found";
     } else {
