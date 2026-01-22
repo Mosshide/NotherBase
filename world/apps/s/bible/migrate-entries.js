6c546e87e053a50e7f7a053520aaf5681b058d2c
Wyatt Lee Sushinsky <wyattsushi@gmail.com>
2026-01-21 20:49:19 -0800

    removed migration

--- world/apps/s/bible/migrate-entries.js
+++
@@ -1,7 +0,0 @@
-export default async (req, user) => {
-    let entries = await req.Spirit.find({ service: "bible-notes" });
-    for (let entry of entries) {
-        if (entry.data) entry.service = "projects";
-        await entry.commit();
-    }
-}
